b9c45805510801a9ac95e217c4a50dbc46844c1e
Anixx <neptunia@mail.ru>
2025-12-08 20:00:19 +0200

    Support for unthemed taskbar
    See this discussion:
    https://github.com/Open-Shell/Open-Shell-Menu/discussions/2367

--- Src/StartMenu/StartMenuDLL/StartMenuDLL.cpp
+++ Src/StartMenu/StartMenuDLL/StartMenuDLL.cpp
@@ -1872,6 +1872,7 @@ static LRESULT CALLBACK SubclassTaskBarProc( HWND hWnd, UINT uMsg, WPARAM wParam
 		ComputeTaskbarColors(data);
 		WINCOMPATTRDATA attrData={0x13,&data,sizeof(data)};
 		SetWindowCompositionAttribute(hWnd,&attrData);
+		if(!IsAppThemed()) {UpdateTaskBars(TASKBAR_UPDATE_TEXTURE);}
 		return res;
 	}
 	if ((uMsg==WM_DWMCOLORIZATIONCOLORCHANGED || uMsg==WM_SETTINGCHANGE) && taskBar && taskBar->bCustomLook && SetWindowCompositionAttribute && GetWinVersion()<WIN_VER_WIN10)
@@ -1932,7 +1933,7 @@ static LRESULT CALLBACK SubclassTaskBarProc( HWND hWnd, UINT uMsg, WPARAM wParam
 				WINCOMPATTRDATA attrData={0x13,&data,sizeof(data)};
 				SetWindowCompositionAttribute(hWnd,&attrData);
 			}
-			if (g_TaskbarTexture && IsAppThemed())
+			if (g_TaskbarTexture)
 			{
 				// draw taskbar background (behind start button and separators)
 				PAINTSTRUCT ps;
@@ -2383,13 +2384,9 @@ void UpdateTaskBars( TUpdateTaskbar update )
 					}
 				}
 			}
-			else if (GetWinVersion()<WIN_VER_WIN10 && (!bDefColor || !bDefOpacity))
+			else
 			{
-				if (bDefColor && GetWinVersion()>WIN_VER_WIN7)
-				{
-					color=GetSystemGlassColor8();
-					color=((color&0xFF)<<16)|(color&0xFF00)|((color>>16)&0xFF);
-				}
+				color=GetSysColor(COLOR_BTNFACE);
 				BITMAPINFO bi={0};
 				bi.bmiHeader.biSize=sizeof(BITMAPINFOHEADER);
 				bi.bmiHeader.biWidth=bi.bmiHeader.biHeight=32;
@@ -3012,7 +3009,10 @@ static void InitStartMenuDLL( void )
 		if (GetWinVersion()<=WIN_VER_WIN81)
 			g_DrawThemeBackgroundHook=SetIatHook(module,"uxtheme.dll","DrawThemeBackground",DrawThemeBackground2);
 		g_DrawThemeTextHook=SetIatHook(module,"uxtheme.dll","DrawThemeText",DrawThemeText2);
-		g_DrawThemeTextExHook=SetIatHook(module,"uxtheme.dll","DrawThemeTextEx",DrawThemeTextEx2);
+		if (IsAppThemed())
+		{
+			g_DrawThemeTextExHook=SetIatHook(module,"uxtheme.dll","DrawThemeTextEx",DrawThemeTextEx2);
+		}
 		g_DrawThemeTextCtlHook=SetIatHook(GetModuleHandle(L"comctl32.dll"),"uxtheme.dll","DrawThemeText",DrawThemeText2);
 		if (GetWinVersion()>=WIN_VER_WIN10)
 			g_SetWindowCompositionAttributeHook=SetIatHook(module,"user32.dll","SetWindowCompositionAttribute",SetWindowCompositionAttribute2);
